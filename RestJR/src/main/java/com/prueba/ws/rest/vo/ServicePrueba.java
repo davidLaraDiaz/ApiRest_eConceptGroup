a3a89e3a332325a772fe78a0cba862cce1adfc6a
Fabian Camilo Gomez Vega <fabian.gomez1@ibm.com>
2019-04-09 07:02:38 -0500

    Metodo auto generar id y carga de json

--- RestJR/src/main/java/com/prueba/ws/rest/vo/ServicePrueba.java
+++ RestJR/src/main/java/com/prueba/ws/rest/vo/ServicePrueba.java
@@ -8,10 +8,22 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+import org.json.simple.JSONArray;
+
 import com.prueba.ws.rest.vo.VoService;
 
 @Path("/ServicePrueba")
 public class ServicePrueba {
+	
+	private JSONArray arrayJson;
+	
+	
+	public ServicePrueba() {
+		
+		arrayJson = new JSONArray();
+		cargaJson();
+		
+	}
 
 	@POST
 	@Path("/prueba")
@@ -20,12 +32,40 @@ public class ServicePrueba {
 	public VoService prueba(VoService vo) {
 		
 		if (vo.getRequest().getPf().getCanal().equals("ECO")) {
-			vo.setResponse(new ResponseClass(new AppClass("0000", "Se ha creado un nuevo usuario"), vo.getResponse().getPv()));
+			
+			UsuarioClass usuario = new UsuarioClass(
+					generarId(),
+					vo.getRequest().getPv().getUsuario().getName(),
+					vo.getRequest().getPv().getUsuario().getPassword(),
+					vo.getRequest().getPv().getUsuario().getEmail()
+					);
+			PvClass pv = new PvClass(usuario, "admin");
+			vo.setResponse(new ResponseClass(new AppClass("0000", "Se ha creado un nuevo usuario"), pv));
+			arrayJson.add(vo);
 		}else{
 			vo.setResponse(new ResponseClass(new AppClass("2222", "Error --- Canal No Reconocido ---"), vo.getResponse().getPv()));
 		}
 		
 		return vo;
+	}
+	
+	public String generarId() {
+		int id=arrayJson.size() + 1;
+		return ""+id;
+		
+	}
+	
+	public void cargaJson(){
+		
+		PfClass pf = new PfClass("ECO", "1234", "20181004", "123", "1.1");
+		UsuarioClass usuario = new UsuarioClass("001", "Juan Prueba", "1234", "juan@321");
+		PvClass pv = new PvClass(usuario, "Admin");
+		RequestClass request = new RequestClass(pf, pv);
+		AppClass app = new AppClass("0000", "Se he creado un nuevo usuario");
+		ResponseClass response = new ResponseClass(app, pv);
+		VoService nuevo = new VoService(request, response);
+		
+		arrayJson.add(nuevo);
 	}
 	
 	
